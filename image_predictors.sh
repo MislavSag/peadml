3531503fc09742723cf8f66a45f75128a0d6c105
MislavSag <mislav.sagovac@contentio.biz>
2024-12-30 15:56:44 +0100

    image predictors

--- image_predictors.sh
+++ image_predictors.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-#PBS -N pread_predictions
-#PBS -l ncpus=1
-#PBS -l mem=4GB
-#PBS -J 1-4599
-#PBS -o logs
-#PBS -j oe
-
-cd ${PBS_O_WORKDIR}
-
-apptainer run image_predictors.sif predictors_padobran.R
+apptainer build image_predictors.sif image_predictors.def
