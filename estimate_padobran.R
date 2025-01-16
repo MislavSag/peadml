25f64c0f73fd74e3a255083c67dfbbd3249a9531
MislavSag <mislav.sagovac@contentio.biz>
2025-01-16 10:56:21 +0100

    change file name

--- estimate_padobran.R
+++ estimate_padobran.R
@@ -52,7 +52,7 @@ if (interactive()) {
                   format = "%Y%m%d")
   DT = fread(pead_file_local[which.max(dates)])
 } else {
-  DT = fread("pead-predictors.csv")
+  DT = fread("predictors.csv")
 }
 
 # Remove industry and sector vars
